349cf7854a08a8671775658368027d6e441f4f4b
bob <rob.draper@live.com>
2020-03-20 13:04:21 +0000

    renamed migrations

--- db/migrations/20200224123259_create-topics-table.js
+++ db/migrations/20200224123259_create-topics-table.js
@@ -4,6 +4,11 @@ exports.up = function(knex) {
     topicsTable.string("description").notNullable();
   });
 };
+//
+//20200203114930_topic
+//20200203124215_comments
+//20200203122331_article
+//20200203121649_user
 
 exports.down = function(knex) {
   return knex.schema.dropTable("topics");
